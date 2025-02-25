e78d02f9b17fba321c85bdabc35a9dafc9fe7292
Rasadul Islam <rasadulislam.cse@gmail.com>
2025-02-25 11:45:43 +0600

    Setup task manager application

---
+++ index.js
@@ -0,0 +1,46 @@
+const express = require('express');
+const cors = require('cors');
+const { MongoClient, ServerApiVersion, ObjectId } = require('mongodb');
+require('dotenv').config();
+const app = express();
+const port = process.env.PORT || 5000;
+
+app.use(express.json());
+app.use(cors());
+
+const uri = `mongodb+srv://${process.env.DB_USER}:${process.env.DB_PASS}@cluster0.dzkk6.mongodb.net/?retryWrites=true&w=majority&appName=Cluster0`;
+
+// MongoDB Client Setup
+const client = new MongoClient(uri, {
+    serverApi: {
+        version: ServerApiVersion.v1,
+        strict: true,
+        deprecationErrors: true,
+    }
+});
+
+async function run() {
+    try {
+
+        // await client.connect();
+
+        const TaskCollection = client.db('task-manager').collection('tasks');
+        const usersCollection = client.db('task-manager').collection('users');
+
+
+
+
+    } catch (error) {
+        console.error("Error connecting to MongoDB:", error);
+    }
+}
+
+run().catch(console.dir);
+
+app.get('/', (req, res) => {
+    res.send("Task Manager server is running");
+});
+
+app.listen(port, () => {
+    console.log(`Task Manager server is running on port: ${port}`);
+});
